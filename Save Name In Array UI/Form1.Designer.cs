deaa859e779cc8da8593347843c2dc41eca097c5
Ramcste <lucky.guyram@gmail.com>
2015-04-25 22:41:50 +0600

    Save Name In Array

---
+++ Save Name In Array UI/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Save_Name_In_Array_UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameAddButton = new System.Windows.Forms.Button();
+            this.showAllButton = new System.Windows.Forms.Button();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameListBox = new System.Windows.Forms.ListBox();
+            this.totalNameTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nameAddButton
+            // 
+            this.nameAddButton.Location = new System.Drawing.Point(274, 60);
+            this.nameAddButton.Name = "nameAddButton";
+            this.nameAddButton.Size = new System.Drawing.Size(75, 23);
+            this.nameAddButton.TabIndex = 0;
+            this.nameAddButton.Text = "Save";
+            this.nameAddButton.UseVisualStyleBackColor = true;
+            this.nameAddButton.Click += new System.EventHandler(this.nameAddButton_Click);
+            // 
+            // showAllButton
+            // 
+            this.showAllButton.Location = new System.Drawing.Point(274, 129);
+            this.showAllButton.Name = "showAllButton";
+            this.showAllButton.Size = new System.Drawing.Size(75, 23);
+            this.showAllButton.TabIndex = 1;
+            this.showAllButton.Text = "Show All";
+            this.showAllButton.UseVisualStyleBackColor = true;
+            this.showAllButton.Click += new System.EventHandler(this.showAllButton_Click);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(129, 60);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(120, 20);
+            this.nameTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter Name:";
+            // 
+            // nameListBox
+            // 
+            this.nameListBox.FormattingEnabled = true;
+            this.nameListBox.Location = new System.Drawing.Point(129, 144);
+            this.nameListBox.Name = "nameListBox";
+            this.nameListBox.Size = new System.Drawing.Size(120, 147);
+            this.nameListBox.TabIndex = 4;
+            // 
+            // totalNameTextBox
+            // 
+            this.totalNameTextBox.Location = new System.Drawing.Point(140, 321);
+            this.totalNameTextBox.Name = "totalNameTextBox";
+            this.totalNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.totalNameTextBox.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 328);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Total:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 374);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.totalNameTextBox);
+            this.Controls.Add(this.nameListBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.showAllButton);
+            this.Controls.Add(this.nameAddButton);
+            this.Name = "Form1";
+            this.Text = "Save Name In Array";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button nameAddButton;
+        private System.Windows.Forms.Button showAllButton;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox nameListBox;
+        private System.Windows.Forms.TextBox totalNameTextBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Save Name In Array UI/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Save_Name_In_Array_UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string name;
+        
+        string [] nameArray=new string[5];
+        
+        List<string > list=new List<string>(); 
+        private void nameAddButton_Click(object sender, EventArgs e)
+        {
+
+
+
+            name=nameTextBox.Text;
+           
+            int totalname = list.Count;
+
+            if (totalname < 5)
+            {
+                list.Add(name);
+                nameArray = list.ToArray();
+
+                nameTextBox.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("U can't add name more than five");
+            }
+
+        }
+
+        private void showAllButton_Click(object sender, EventArgs e)
+        {
+            //nameListBox.DataSource = list;
+
+            foreach (string s in nameArray)
+            {
+                nameListBox.Items.Add(s);
+            }
+            totalNameTextBox.Text = nameListBox.Items.Count.ToString();
+
+        }
+    }
+}
